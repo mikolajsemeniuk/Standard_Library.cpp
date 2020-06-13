c87edfebd5a71de297159ebf01af0b39fc88fe55
mikolajs123 <53497782+mikolajs123@users.noreply.github.com>
2020-06-13 19:28:52 +0200

    Update map.cpp

--- map.cpp
+++ map.cpp
@@ -1,1 +1,19 @@
+//
+//  main.cpp
+//  Temp_C++
+//
+//  Created by Mikołaj Semeniuk on 04/04/2020.
+//  Copyright © 2020 Mikołaj Semeniuk. All rights reserved.
+//
+
+
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    cout << "hi" << endl;
+    return 0;
+}
 
